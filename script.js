14c11540546e99185b047858d78913fa10fb54a7
lumina-wellness <noushadmajitha45@gmail.com>
2026-01-16 20:27:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function submitStress() {
+  document.getElementById("stressResult").innerText =
+    "Stress recorded anonymously. Take care 💙";
+}
+
+function submitIssue() {
+  document.getElementById("issueResult").innerText =
+    "Your report was sent safely and anonymously.";
+}
